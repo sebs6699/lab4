bcb4cb4e6a3001db5aa842555ec7fdefa9d9a929
Sebastian Tipan <stipanso@my.centennialcollege.ca>
2025-04-01 17:59:10 -0400

    Initial commit - StringExtensions with StartWithUpper method

---
+++ StringExtensionsLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return false;
+
+            return char.IsUpper(input[0]);
+        }
+    }
+}
